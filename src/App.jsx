5464b056b3ac005e0a3e7f491ca29f6ddb1a1da1
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-03-26 01:38:52 -0500

    mejor forma de recuperar los datos de un form

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
-import './App.css'
-import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
+import { Movies } from './components/Movies'
+// import { useRef } from 'react' // permite crear una referencia estable y mutable que no cambia en cada renderizado
+import './App.css'
 
 export function App () {
-  const { movies: mappedMovies } = useMovies()
+  const { movies } = useMovies()
+  // const inputRef = useRef()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const fields = Object.fromEntries(new window.FormData(e.target))
+    console.log(fields)
+  }
 
   return (
     <section className='page'>
 
       <header>
         <h1>Buscador De Peliculas</h1>
-        <form className="form">
-          <input type="text" placeholder="Avenger, Star Wars, The Matrix" />
+        <form className="form" onSubmit={handleSubmit}>
+          <input name="query" type="text" placeholder="Avenger, Star Wars, The Matrix" />
+          <input name="name" type="text" placeholder="Avenger, Star Wars, The Matrix" />
+          <input name="lastname" type="text" placeholder="Avenger, Star Wars, The Matrix" />
+          <input name="entris" type="text" placeholder="Avenger, Star Wars, The Matrix" />
           <button type="submit">Buscar</button>
         </form>
       </header>
 
       <main>
-        <Movies movies={mappedMovies} />
+        <Movies movies={movies} />
       </main>
 
     </section >
