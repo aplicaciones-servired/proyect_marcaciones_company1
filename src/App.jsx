78b78bc8009d1d968c6bde99d3866a54357e42eb
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-04-23 22:52:43 -0500

    falta añadir tareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { tasks as data} from "./task.js";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Appjs() {
+  const [tasks, setTask] = useState([]);
+
+  useEffect(() => {
+    setTask(data);
+  }, []);
+
+  if (tasks.length === 0) {
+    return <h1>No Tengo Tareas</h1>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div key={tasks.id}>
+      {tasks.map((task) => (
+        <div>
+          <h1>{task.title}</h1>
+          <p>{task.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default Appjs;
---
+++ src/task.js
@@ -0,0 +1,27 @@
+export const tasks = [
+  {
+    id: 1,
+    title: 'Mi Primer Tarea',
+    description: 'Iniciando 1° Tarea'
+  },
+  {
+    id: 2,
+    title: 'Mi Segunda Tarea',
+    description: 'Iniciando 2° Tarea'
+  },
+  {
+    id: 3,
+    title: 'Mi Tercer Tarea',
+    description: 'Iniciando 3° Tarea'
+  },
+  {
+    id: 4,
+    title: 'Mi Cuarta Tarea',
+    description: 'Iniciando 4° Tarea'
+  },
+  {
+    id: 5,
+    title: 'Mi Quinta Tarea',
+    description: 'Iniciando 5° Tarea'
+  }
+];
