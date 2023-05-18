76725aaa70487ae47c6556bfd1f9d7da972672fe
llOrtegall <IvanOrtega_97@hotmail.com>
2023-05-18 12:25:31 -0500

    creando el use context global

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Products } from "./components/Products.jsx";
 import { products as initialProducts } from "./mocks/products.json";
 import { Header } from "./components/Header.jsx";
 import { Footer } from "./components/Footer.jsx";
 import { IS_DEVELOPMENT } from "./config.js";
+import { FiltersContext } from "./context/filters.jsx"
 
 function useFilters() {
-  const [filters, setFilters] = useState({
-    category: "all",
-    minPrice: 0,
-  });
+
+  const {filters, setFilters} = useContext(FiltersContext)
+
   const filterProducts = (products) => {
     return products.filter((product) => {
       return (
---
+++ src/context/filters.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState } from "react";
+
+// 1. crear el Context
+export const FiltersContext = createContext();
+
+// 2. crear el Provider para preveer el context
+
+export function FiltersProvider({ children }) {
+  const [filters, setFilters] = useState({
+    category: "all",
+    minPrice: 0,
+  });
+
+  return (
+    <FiltersContext.Provider
+      value={{
+        filters,
+        setFilters,
+      }}
+    >
+      {children}
+    </FiltersContext.Provider>
+  );
+}
