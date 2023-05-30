20ecc5f2f278e331164fb83adbced61e172ce790
llOrtegall <IvanOrtega_97@hotmail.com>
2023-05-30 10:30:06 -0500

    agregando funcion agregar eliminar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,20 @@
 
+import { useCart } from '../hooks/useCart';
 import { useFilters } from '../hooks/useFilters';
 import './Footer.css';
 
-export function Footer(){
-    const { filters } = useFilters();
-    return(
-        <footer className='footer'>
-            {
-                JSON.stringify(filters, null, 2)
-            }
-            {/* <h4>Prueba técnica React</h4>
+export function Footer() {
+	const { filters } = useFilters();
+	const { cart } = useCart();
+
+	return (
+		<footer className='footer'>
+			{
+				JSON.stringify(cart, null, 2)
+			}
+			{/* <h4>Prueba técnica React</h4>
             <span>@lOrTeGal</span>
             <h5>Shopping Cart con useContext & useReduce</h5> */}
-        </footer>
-    )
+		</footer>
+	)
 }
