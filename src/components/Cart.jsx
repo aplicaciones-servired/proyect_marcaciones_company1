2a284f1fa16fb60f4f35e19cec07a884823271b2
llOrtegall <IvanOrtega_97@hotmail.com>
2023-05-30 11:09:45 -0500

    agregando item al carro termianda

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,31 +1,51 @@
 import "./Cart.css"
 import { useId } from "react";
-import { CartIcon,  ClearCartIcon } from "../components/Icons.jsx";
+import { CartIcon, ClearCartIcon } from "../components/Icons.jsx";
+import { useCart } from '../hooks/useCart.js'
+
+function CarItem({ thumbnail, price, title, quantity, addToCart }) {
+
+	return (
+		<li>
+			<img src={thumbnail}
+				alt={title} />
+			<div>
+				<strong>{title}</strong> - ${price}
+			</div>
+			<footer>
+				<small>Qty: {quantity}</small>
+				<button onClick={addToCart}> + </button>
+			</footer>
+		</li>
+	)
+}
 
 export function Cart() {
 	const carCheckboxId = useId();
+	const { cart, clearCart, addToCart } = useCart();
+
+
+
 	return (
 		<>
 			<label className="cart-button" htmlFor={carCheckboxId}>
 				<CartIcon />
 			</label>
-			<input id={carCheckboxId} type="checkbox" hidden/>
+			<input id={carCheckboxId} type="checkbox" hidden />
 
 			<aside className="cart">
 				<ul>
-					<li>
-						<img src="https://i.dummyjson.com/data/products/2/thumbnail.jpg"
-						alt="Iphone" />
-						<div>
-							<strong>iPhone</strong> - $1499
-						</div>
-						<footer>
-							<small>Qty: 1</small>
-							<button> + </button>
-						</footer>
-					</li>
+					{
+						cart.map(product => (
+							<CarItem key={product.id}
+								addToCart={() => addToCart(product)}
+								{...product} />
+						))
+					}
 				</ul>
-					<button><ClearCartIcon/></button>
+				<button onClick={clearCart}>
+					<ClearCartIcon />
+				</button>
 			</aside>
 		</>
 	)
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,13 @@
 
-import { useCart } from '../hooks/useCart';
-import { useFilters } from '../hooks/useFilters';
 import './Footer.css';
 
 export function Footer() {
-	const { filters } = useFilters();
-	const { cart } = useCart();
 
 	return (
 		<footer className='footer'>
-			{
-				JSON.stringify(cart, null, 2)
-			}
-			{/* <h4>Prueba técnica React</h4>
-            <span>@lOrTeGal</span>
-            <h5>Shopping Cart con useContext & useReduce</h5> */}
+			<h4>Prueba técnica React</h4>
+			<span>@lOrTeGal</span>
+			<h5>Shopping Cart con useContext & useReduce</h5>
 		</footer>
 	)
 }
