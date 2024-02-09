a555249c397366c242d98bdd5fa89c65784ab6f6
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-02-09 12:12:21 -0500

    mejorar performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { DonutChartUsageExample2 } from './components/Donout.jsx'
+import { HardwareOSC } from './services/hardware.js'
 
 
 function App() {
+  HardwareOSC()
   return (
     <section className="w-[100vw] h-[100vh] flex items-center justify-center">
       <DonutChartUsageExample2 />
--- src/components/Donout.jsx
+++ src/components/Donout.jsx
@@ -1,49 +1,35 @@
 import { Card, DonutChart, Title, List, ListItem } from "@tremor/react"
 import { useState } from "react";
-const systems = [
-  {
-    name: "Windows",
-    acount: 22,
-  },
-  {
-    name: "Mac-OS",
-    acount: 15,
-  },
-  {
-    name: "Linux",
-    acount: 30,
-  },
-  {
-    name: "Android",
-    acount: 20,
-  }
-];
+
+import { HardwareOSC } from "../services/hardware"
 
 export const DonutChartUsageExample2 = () => {
   const [value, setValue] = useState(null);
 
+  const Sistemas = HardwareOSC()
+
   return (
     <>
-      <Card className="flex w-[520px] gap-4 justify-around">
+      <Card className="flex w-[540px] gap-4 justify-around" >
         <section>
           <Title>Sistemas Operativos</Title>
           <DonutChart
             className="mt-6"
-            data={systems}
-            category="acount"
+            data={Sistemas}
+            category="count"
             index="name"
             colors={["blue", "gray", "red", "yellow"]}
             onValueChange={(v) => setValue(v)}
           />
         </section>
 
-        <section>
+        <section className="flex flex-col justify-between">
           <Title>Detallado Sistemas</Title>
           <List>
-            {systems.map((item) => (
-              <ListItem key={item.city}>
-                <p><span>{item.name}</span></p>
-                <p><span>{item.acount}</span></p>
+            {Sistemas.map((item, index) => (
+              <ListItem key={index}>
+                <p className="font-semibold text-white pr-4"><span>{item.name}</span></p>
+                <p className="font-semibold text-white pr-4"><span>{item.count}</span></p>
               </ListItem>
             ))}
           </List>
