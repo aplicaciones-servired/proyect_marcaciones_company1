430d1bb31af2947a1a9d83231c69d6cd9770fc3f
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-02-09 11:18:40 -0500

    Tremor instalar

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,19 @@
-import { useMovies } from './hooks/useMovies'
-import { Movies } from './components/Movies'
-// import { useRef } from 'react' // permite crear una referencia estable y mutable que no cambia en cada renderizado
-import './App.css'
+import { Card, Text, Metric, Flex, ProgressBar } from "@tremor/react";
 
-export function App () {
-  const { movies } = useMovies()
-  // const inputRef = useRef()
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const fields = Object.fromEntries(new window.FormData(e.target))
-    console.log(fields)
-  }
-
+function App() {
   return (
-    <section className='page'>
-
-      <header>
-        <h1>Buscador De Peliculas</h1>
-        <form className="form" onSubmit={handleSubmit}>
-          <input name="query" type="text" placeholder="Avenger, Star Wars, The Matrix" />
-          <input name="name" type="text" placeholder="Avenger, Star Wars, The Matrix" />
-          <input name="lastname" type="text" placeholder="Avenger, Star Wars, The Matrix" />
-          <input name="entris" type="text" placeholder="Avenger, Star Wars, The Matrix" />
-          <button type="submit">Buscar</button>
-        </form>
-      </header>
-
-      <main>
-        <Movies movies={movies} />
-      </main>
-
-    </section >
+    <section className="w-[100vw] h-[100vh] flex items-center justify-center">
+      <Card className="max-w-xs mx-auto">
+        <Text>Sales</Text>
+        <Metric>$ 71,465</Metric>
+        <Flex className="mt-4">
+          <Text>32% of annual target</Text>
+          <Text>$ 225,000</Text>
+        </Flex>
+        <ProgressBar value={32} className="mt-2" />
+      </Card>
+    </section>
   )
 }
+
+export default App
