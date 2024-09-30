48aa2484c9f4e8e259650a31ab6c245c92a3a610
llOrtegall <IvanOrtega_97@hotmail.com>
2024-09-30 13:02:36 -0500

    Refactor database configuration to use environment variables

--- server/src/connections/index.ts
+++ server/src/connections/index.ts
@@ -1,10 +1,15 @@
-import { DB_NAME, DB_HOST, DB_PASS, DB_PORT, DB_USER, DB_DIALECT } from '../config';
 import { Sequelize } from 'sequelize';
+
+const DB_NAME = process.env.DB_NAME!;
+const DB_USER = process.env.DB_USER!;
+const DB_PASS = process.env.DB_PASS!;
+const DB_HOST = process.env.DB_HOST!;
+const DB_PORT = process.env.DB_PORT!;
 
 const db_connection = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
   host: DB_HOST,
   port: parseInt(DB_PORT),
-  dialect: DB_DIALECT === 'true' ? 'mysql' : 'mariadb',
+  dialect: 'mariadb',
   timezone: '-05:00'
 });
 
