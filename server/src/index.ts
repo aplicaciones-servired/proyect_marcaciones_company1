c5c9a54f9e0c3b9e1ada4672a679974f7679ee55
llOrtegall <IvanOrtega_97@hotmail.com>
2024-09-30 11:08:51 -0500

    esta implementación funciona validar todas las rutas

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,16 +3,22 @@ import express from 'express';
 import logs from 'morgan';
 import cors from 'cors';
 
+import { infoRoutes } from './routes/info.routes';
+import { marcacionRouter } from './routes/marcacion.routes';
+import { opcionesRouter } from './routes/opciones.routes';
+import { personaRouter } from './routes/persona.routes';
+
 const v1 = '/api/marcaciones/v1';
 const app = express();
 
-app.use(cors({ origin: CORS }));
+app.use(cors({ origin: CORS, credentials: true }));
 app.use(express.json());
 app.use(logs('dev'));
 
-app.get(v1, (req, res) => {
-  res.send('Hello World');
-});
+app.use(v1, infoRoutes);
+app.use(v1, marcacionRouter);
+app.use(v1, opcionesRouter);
+app.use(v1, personaRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
