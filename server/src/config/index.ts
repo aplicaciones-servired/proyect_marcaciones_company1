2b47d1e6b47e6a49d9556b9dfd91a55573088bd2
llOrtegall <IvanOrtega_97@hotmail.com>
2024-09-30 09:46:25 -0500

    variables de entorno agregadas

---
+++ server/src/config/index.ts
@@ -0,0 +1,10 @@
+export const PORT = process.env.PORT!;
+
+export const DB_NAME = process.env.DB_MAR_NAME!;
+export const DB_USER = process.env.DB_MAR_USER!;
+export const DB_PASS = process.env.DB_MAR_PASS!;
+export const DB_HOST = process.env.DB_MAR_HOST!;
+export const DB_PORT = process.env.DB_MAR_PORT!;
+export const DB_DIALECT = process.env.DB_MAR_DIALECT!;
+
+export const CORS = process.env.CORS_ORIGINS!;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello, world!');
+import { PORT } from './config';
+import express from 'express';
+
+const v1 = '/api/marcaciones/v1';
+const app = express();
+
+app.get(v1, (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
