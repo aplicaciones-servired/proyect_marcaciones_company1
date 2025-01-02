9f64f66a2ba5b21611edc23045b5a9eccb85609f
llOrtegall <IvanOrtega_97@hotmail.com>
2025-01-02 09:03:26 -0500

    componentes agragados y/o actualizados y package add dependencies

--- client/src/components/ui/Input.tsx
+++ client/src/components/ui/Input.tsx
@@ -1,7 +1,22 @@
-interface InputProps extends React.HTMLProps<HTMLInputElement> {}
+import * as React from "react"
 
-export const Input = ({...props }: InputProps) => {
-  return (
-    <input className="py-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" {...props} />
-  )
-}
+import { cn } from "@/lib/utils"
+
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Input.displayName = "Input"
+
+export { Input }
